c48599a5a87cadddb6930e1efb4dc4a104cd2442
Siumbel <sumbelragipova@gmail.com>
2022-05-01 12:20:57 +0300

    second commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,8 @@
 public class BonusMilesService {
 
     public int calculate(int price) {
-        int calculate = price / 2;
+        int calculate = price / 20; //за каждые 20 рублей, потраченные на билет, начисляется 1 миля
+
         return calculate;
     }
 }
